07b33dd8788680ed17085686c0aedf09ab97463a
ethereal-squirrel <ryan.luxi.dev@gmail.com>
2025-07-19 13:14:58 +0100

    Initial rating and tag support.

---
+++ migrations/8.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "UserRating" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "file_type" TEXT NOT NULL,
+    "file_id" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL
+);
+
+CREATE TABLE "UserTag" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "file_type" TEXT NOT NULL,
+    "file_id" INTEGER NOT NULL,
+    "rating" TEXT NOT NULL
+);
